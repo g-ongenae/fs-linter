bb7ac995cd8030a5f5a990e1922c2f5545516c18
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-04-08 04:57:11 +0200

    Add launch script

---
+++ bin/fs-linter.js
@@ -0,0 +1,38 @@
+#! /bin/env/node
+
+'use strict';
+
+const program = require('commander');
+const { resolve } = require('path');
+const { version, description } = require('../package.json');
+const { readRecursivelyDirectory } = require('../src/readFiles');
+const { applyEachRule } = require('../src/applyRules');
+// const { prettifyOutput } = require('../src/prettifyOutput');
+
+function list (val) {
+	return val.split(',');
+}
+
+async function main (directory, rules, options) {
+	const treeView = await readRecursivelyDirectory(directory);
+	const problems = await applyEachRule(rules, treeView, options);
+	// const output = await prettifyOutput(problems);
+	console.log(problems); // eslint-disable-line
+}
+
+program
+	.version(version)
+	.description(description)
+	.usage('fs-linter -c fsConfig.json -t .')
+	.option('-d, --directory <dir>', 'Directory to be lint')
+	.option('-r, --rules <rulesFiles>', 'The list of rules ex: rule1,rule2', list)
+	.option('-o, --options <options>', 'List of options for the rules', JSON.parse)
+	.parse(process.argv); // eslint-disable-line
+
+if (program.directory && program.rules) {
+	const opt = program.option || {};
+	const dir = resolve(program.directory);
+	main(dir, program.rules, opt);
+} else {
+	console.error('Directory and rules need to be set.'); // eslint-disable-line
+}
